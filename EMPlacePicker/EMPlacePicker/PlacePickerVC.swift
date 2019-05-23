9bb2633a3cda18d5f700df42afb34f9d53277f1a
Lahiru Chathuranga <hiru.wlc@gmail.com>
2019-05-23 09:53:17 +0530

    V0.1.9

--- EMPlacePicker/EMPlacePicker/PlacePickerVC.swift
+++ EMPlacePicker/EMPlacePicker/PlacePickerVC.swift
@@ -68,7 +68,7 @@ open class PlacePickerVC: UIViewController, FloatingPanelControllerDelegate {
         getPlacesAccordingToUserLocation()
     }
     
-    public func getPlacesAccordingToUserLocation() {
+     func getPlacesAccordingToUserLocation() {
         getLocationUsingCoordinate(lat: userLocation.coordinate.latitude , lon: userLocation.coordinate.longitude ) {
             self.tableView.reloadData()
         }
@@ -263,6 +263,96 @@ extension PlacePickerVC: UITableViewDelegate, UITableViewDataSource {
         self.dismissView()
     }
 }
+
+class PickerTVCell: UITableViewCell {
+    
+    let placeNameLabel: UILabel = {
+        let placeName = UILabel()
+        placeName.text = "Place name"
+        placeName.numberOfLines = 0
+        placeName.font = UIFont(name: "SF Pro Display", size: 16)
+        placeName.textColor = UIColor.white  //UIColor(red:0.03, green:0.15, blue:0.26, alpha:1)
+        return placeName
+    }()
+    
+    let upperView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    func InitialSetup () {
+        addSubViews()
+        setConstraints()
+        setupUI()
+    }
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func setupUI() {
+        contentView.backgroundColor = UIColor.clear
+        upperView.backgroundColor = UIColor(red:0.03, green:0.15, blue:0.26, alpha:1)
+        upperView.layer.cornerRadius = 4
+    }
+    
+    func addSubViews() {
+        contentView.addSubview(upperView)
+        contentView.addSubview(placeNameLabel)
+        //        contentView.addSubview(coordinateLabel)
+    }
+    
+    func setConstraints() {
+        upperView.snp.makeConstraints { (make) in
+            make.left.equalTo(self.contentView).offset(5)
+            make.right.equalTo(self.contentView).offset(-5)
+            make.top.equalTo(self.contentView).offset(5)
+            make.bottom.equalTo(self.contentView).offset(-5)
+        }
+        placeNameLabel.snp.makeConstraints { (make) in
+            make.left.equalTo(self.upperView).offset(10)
+            make.right.equalTo(self.upperView).offset(-10)
+            make.top.equalTo(self.upperView).offset(10)
+            make.bottom.equalTo(self.upperView).offset(-10)
+            make.centerX.equalTo(self.upperView.snp.centerX)
+            
+        }
+    }
+    
+}
+class LocationManager: NSObject, CLLocationManagerDelegate {
+    
+    static let shared = LocationManager()
+    private var manager: CLLocationManager!
+    var userLocation: CLLocation?
+    
+    override init() {
+        super.init()
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+    }
+    
+    func determineCurrentLocation() {
+        if CLLocationManager.locationServicesEnabled() {
+            manager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let _userLocation: CLLocation = locations[0] as CLLocation
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        userLocation = (_userLocation)
+        manager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        //
+    }
+}
+
+
 
 
 
